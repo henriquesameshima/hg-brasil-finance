98b8814ebe9e813b3588b16dee61c9bc7ae740fa
Henrique Sameshima <sameshima@live.com>
2023-10-19 15:13:21 -0300

    Fix invalid key

--- src/main/java/io/sameshima/hgbrasil/service/api/RetrofitClient.java
+++ src/main/java/io/sameshima/hgbrasil/service/api/RetrofitClient.java
@@ -3,6 +3,7 @@ package io.sameshima.hgbrasil.service.api;
 import java.io.File;
 import java.io.IOException;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
@@ -14,7 +15,9 @@ import io.sameshima.hgbrasil.utils.DeserializadorDefault;
 import okhttp3.Cache;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
@@ -28,6 +31,27 @@ public final class RetrofitClient {
 	private static final String BASE_URL = "https://api.hgbrasil.com/finance/";
 
 	private static Retrofit retrofit = null;
+
+	private static Interceptor responseInterceptor() {
+		return new Interceptor() {
+			@Override
+			public Response intercept(Chain chain) throws IOException {
+				Request request = chain.request();
+
+				Response originalResponse = chain.proceed(request);
+				String responseBodyString = originalResponse.body().string();
+
+				JsonNode jsonNode = new ObjectMapper().readTree(responseBodyString).get("results");
+
+				if(jsonNode.has("error") && jsonNode.get("error").asBoolean()) {
+					throw new IOException(jsonNode.get("message").asText());
+				}
+
+				ResponseBody newResponseBody = ResponseBody.create(originalResponse.body().contentType(), responseBodyString);
+				return originalResponse.newBuilder().body(newResponseBody).build();
+			}
+		};
+	}
 
 	private static Interceptor createCacheInterceptor(final int cacheDurationInSeconds) {
 		return new Interceptor() {
@@ -48,6 +72,7 @@ public final class RetrofitClient {
 			Cache cache = new Cache(httpCacheDirectory, cacheSize);
 
 			OkHttpClient.Builder okHttpClientBuilder = new OkHttpClient.Builder();
+			okHttpClientBuilder.addInterceptor(responseInterceptor());
 
 			if (cacheConfig != null) {
 				okHttpClientBuilder.addNetworkInterceptor(createCacheInterceptor(cacheConfig.cacheDurationInSeconds()))
